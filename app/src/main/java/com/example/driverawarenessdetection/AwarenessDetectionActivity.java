dad4a2fa8977858b040d937aaeffe24b842ed493
Ella475 <Ellashalom1995@gmail.com>
2022-12-07 23:52:53 +0200

    change progressbar color when sleepy

--- app/src/main/java/com/example/driverawarenessdetection/AwarenessDetectionActivity.java
+++ app/src/main/java/com/example/driverawarenessdetection/AwarenessDetectionActivity.java
@@ -1,11 +1,13 @@
 package com.example.driverawarenessdetection;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.driverawarenessdetection.video_processing.awareness_detection.AwarenessManager;
@@ -19,13 +21,14 @@ import java.util.Objects;
 
 
 public class AwarenessDetectionActivity extends AppCompatActivity {
-    private int awarenessPercentage = 0;
+    private int awarenessPercentage = 100;
     private CircularProgressBar circularProgressBar;
     private TextView awarenessPercentageText;
     private CameraSourceWrapper csw;
     private final Handler handler = new Handler();
     private final int updateEvery = 5000; // 1000 milliseconds == 1 second
     private final long animationDuration = 3000;
+    private int percentageCutOff = 20;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +48,13 @@ public class AwarenessDetectionActivity extends AppCompatActivity {
         HashMap<Integer, AwarenessManager> managerHashMap = csw.awarenessProcessor.onSuccessDetector.awarenessHashMap;
         if (!managerHashMap.isEmpty())
             awarenessPercentage = (int) (100 * Objects.requireNonNull(managerHashMap.get(0)).getAwareProbability());
+        if (awarenessPercentage < percentageCutOff) {
+            this.circularProgressBar.setBackgroundProgressBarColor(getColor(0xFF, 0xFF, 0x00, 0x00));
+        }
+        else {
+            this.circularProgressBar.setBackgroundProgressBarColor(getColor(0xFF, 0xA4, 0xAF, 0xF4));
+        }
+
         this.circularProgressBar.setProgressWithAnimation((float) awarenessPercentage, animationDuration);
         this.awarenessPercentageText.setText(awarenessPercentage + "%");
     }
@@ -73,7 +83,7 @@ public class AwarenessDetectionActivity extends AppCompatActivity {
     }
 
     public void finishActivity() {
-//        csw.stop();
+        csw.stop();
         finish();
     }
 
@@ -81,4 +91,8 @@ public class AwarenessDetectionActivity extends AppCompatActivity {
         finishActivity();
     }
 
+    @ColorInt
+    private int getColor(int A, int R, int G, int B) {
+        return  (A & 0xff) << 24 | (R & 0xff) << 16 | (G & 0xff) << 8 | (B & 0xff);
+    }
 }
